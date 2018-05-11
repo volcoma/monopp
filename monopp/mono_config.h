0edf7279b795efe6f59c10c114fae6b6971c2fe5
volcoma <nikolai.p.ivanov@gmail.com>
2018-05-11 22:17:56 +0300

    removed unnecessary files

--- monopp/mono_config.h
+++ monopp/mono_config.h
@@ -4,11 +4,3 @@
 #include <string>
 #include <cassert>
 #include <stdexcept>
-
-#include "common/platform/export.hpp"
-
-#if ETH_ON(ETH_PLATFORM_WINDOWS)
-#ifndef MONO_DLL_IMPORT
-#define MONO_DLL_IMPORT
-#endif
-#endif
--- monopp/mono_type_traits.h
+++ monopp/mono_type_traits.h
@@ -1,6 +1,8 @@
 #pragma once
-
-#include "common/nonstd/traits/function_traits.hpp"
+#include <cstddef>
+#include <functional>
+#include <tuple>
+#include <type_traits>
 
 namespace mono
 {
@@ -8,6 +10,92 @@ namespace mono
 template <typename T>
 using is_mono_valuetype = std::is_pod<T>;
 
+
+template <typename Functor>
+struct function_traits : public function_traits<decltype(&Functor::operator())>
+{
+};
+
+template <typename R, typename... Args>
+struct function_traits<R(Args...)>
+{
+	using return_type = R;
+	using function_type = R(Args...);
+	constexpr static const std::size_t arity = sizeof...(Args);
+};
+
+template <typename R, typename... Args>
+struct function_traits<R (*)(Args...)> : public function_traits<R(Args...)>
+{
+};
+
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...)> : public function_traits<R(Args...)>
+{
+	using owner_type = C&;
+};
+
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...) const> : public function_traits<R(Args...)>
+{
+	using owner_type = const C&;
+};
+
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...) volatile> : public function_traits<R(Args...)>
+{
+	using owner_type = volatile C&;
+};
+
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...) const volatile> : public function_traits<R(Args...)>
+{
+	using owner_type = const volatile C&;
+};
+
+template <typename Functor>
+struct function_traits<std::function<Functor>> : public function_traits<Functor>
+{
+};
+
 template <typename T>
-using function_traits = nonstd::function_traits<T>;
+struct function_traits<T&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<const T&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<volatile T&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<const volatile T&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<T&&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<const T&&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<volatile T&&> : public function_traits<T>
+{
+};
+
+template <typename T>
+struct function_traits<const volatile T&&> : public function_traits<T>
+{
+};
+
 }
